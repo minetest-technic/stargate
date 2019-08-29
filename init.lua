5ca58fa3d743eb908167101fb577314ba93f408d
Panquesito7 <halfpacho@gmail.com>
2019-08-28 19:40:50 -0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,9 +1,9 @@
--- Minetest 0.4.5 : stargate
+-- Minetest mod for 5.0.0+: stargate
 
---data tables definitions
-stargate={}
+-- Data tables definitions
+stargate = {}
 stargate_network = {}
 
-modpath=minetest.get_modpath("stargate")
-dofile(modpath.."/stargate_gui.lua")
-dofile(modpath.."/gate_defs.lua")
+modpath = minetest.get_modpath("stargate")
+dofile(modpath .. "/stargate_gui.lua")
+dofile(modpath .. "/gate_defs.lua")
